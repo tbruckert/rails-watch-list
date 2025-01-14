11fc1e924ff3513f4a0c409087bf82413e4a62ef
tbruckert <info@thibautbruckert.com>
2025-01-14 20:11:13 +0400

    Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,26 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'open-uri'
+require 'json'
+
+puts "Cleaning database..."
+Movie.destroy_all
+
+puts "Fetching movies from TMDB..."
+url = "https://tmdb.lewagon.com/movie/top_rated"
+movies_serialized = URI.open(url).read
+movies = JSON.parse(movies_serialized)
+
+puts "Creating movies..."
+movies['results'].each do |movie|
+  Movie.create!(
+    title: movie['title'],
+    overview: movie['overview'],
+    poster_url: "https://image.tmdb.org/t/p/original#{movie['poster_path']}",
+    rating: movie['vote_average']
+  )
+end
+
+puts "Seed finished! Created #{Movie.count} movies."
